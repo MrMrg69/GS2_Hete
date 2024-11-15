ef83fd621438970e617a893103c5a5c4473f9086
MrMrg69 <mr.viniciusmrg@gmail.com>
2024-11-15 11:52:19 -0300

    O Postman funciona

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,4 +1,24 @@
+// routes/auth.js
+const express = require('express');
+const router = express.Router(); // Aqui é onde o objeto router é definido
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+// Registro de usuário
+router.post('/register', async (req, res) => {
+  const { nome, sobrenome, dataNascimento, email, senha } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: 'Usuário já existe' });
+    }
+    user = new User({ nome, sobrenome, dataNascimento, email, senha });
+    await user.save();
+    res.status(201).json({ message: 'Usuário registrado com sucesso' });
+  } catch (error) {
+    res.status(500).json({ message: 'Erro no servidor' });
+  }
+});
 
 // Login de usuário
 router.post('/login', async (req, res) => {
@@ -8,15 +28,11 @@ router.post('/login', async (req, res) => {
     if (!user || !(await user.comparePassword(senha))) {
       return res.status(400).json({ message: 'Credenciais inválidas' });
     }
-    
-    // Verifique se a chave secreta está definida antes de gerar o token
-    if (!process.env.SECRET_KEY) {
-        return res.status(500).json({ message: 'Chave secreta não configurada no servidor.' });
-    }
-
     const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, { expiresIn: '1h' });
     res.json({ token, message: 'Login bem-sucedido' });
   } catch (error) {
     res.status(500).json({ message: 'Erro no servidor' });
   }
 });
+
+module.exports = router; // Aqui o router é exportado para ser usado em server.js
--- server/src/routes/cars.js
+++ server/src/routes/cars.js
@@ -25,4 +25,30 @@ router.get('/', auth, async (req, res) => {
   }
 });
 
+// Obter detalhes de um carro específico
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const car = await Car.findOne({ _id: req.params.id, usuarioId: req.user.userId });
+    if (!car) {
+      return res.status(404).json({ message: 'Carro não encontrado' });
+    }
+    res.json(car);
+  } catch (error) {
+    res.status(500).json({ message: 'Erro no servidor' });
+  }
+});
+
+// Excluir um carro
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const car = await Car.findOneAndDelete({ _id: req.params.id, usuarioId: req.user.userId });
+    if (!car) {
+      return res.status(404).json({ message: 'Carro não encontrado' });
+    }
+    res.json({ message: 'Carro excluído com sucesso' });
+  } catch (error) {
+    res.status(500).json({ message: 'Erro no servidor' });
+  }
+});
+
 module.exports = router;
